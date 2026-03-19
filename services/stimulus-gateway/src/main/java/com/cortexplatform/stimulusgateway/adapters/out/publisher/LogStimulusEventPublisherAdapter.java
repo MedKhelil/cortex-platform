e3d034763656630dbfe1154a0ef102c26080aa00
Mohamed Khelil <mohamed.khalil@esprit.tn>
2026-03-19 17:29:13 +0100

    comment

---
+++ services/stimulus-gateway/src/main/java/com/cortexplatform/stimulusgateway/adapters/out/publisher/LogStimulusEventPublisherAdapter.java
@@ -0,0 +1,22 @@
+package com.cortexplatform.stimulusgateway.adapters.out.publisher;
+
+import com.cortexplatform.common.events.DomainEventMetadata;
+import com.cortexplatform.stimulusgateway.application.ports.out.StimulusEventPublisherPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogStimulusEventPublisherAdapter implements StimulusEventPublisherPort {
+
+    private static final Logger log = LoggerFactory.getLogger(LogStimulusEventPublisherAdapter.class);
+
+    @Override
+    public void publish(DomainEventMetadata metadata) {
+        log.info("Publishing stimulus event: eventId={}, type={}, version={}, correlationId={}",
+                metadata.eventId().value(),
+                metadata.eventType(),
+                metadata.eventVersion(),
+                metadata.correlationId().value()
+                //Comment
+        );
+    }
+}
